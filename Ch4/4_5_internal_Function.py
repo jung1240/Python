e34a636e58bd8a22b698e8b72f17c3cc95217832
jung1240 <sinue6308@naver.com>
2020-12-23 15:13:32 +0900

    2020/12/23 파이썬 수업예제

--- Ch4/4_5_internal_Function.py
+++ Ch4/4_5_internal_Function.py
@@ -3,6 +3,10 @@
 이름 : 정유락
 내용 : 파이썬 내장함수 실습하기
 """
+import time
+import calendar as cal
+import math, random
+
 
 # all() : 리스트에서 0이 포함되었는지 확인
 list1 = [1, 2, 3, 4, 5]
@@ -29,14 +33,74 @@ list5 = ['김유신', '김춘추', '장보고']
 for i, name in enumerate(list5):
     print(i, name)
 
+# lambda : 람다식을 정의하는 함수
+def sum1(a, b):
+    return a+b
+sum2 = lambda a, b: a+b
 
+rs1 = sum1(2, 3)
+rs2 = sum1(2, 3)
 
-# lambda
-# time()
-# time()
-# calendar()
+print('rs1 : ', rs1)
+print('rs2 : ', rs2)
+
+# time() : 날짜/시간 구하는 함수
+t1 = time.time()
+print('t1 : ', t1)
+
+t2 = time.ctime()
+print('t2 : ', t2)
+
+now = time.localtime(time.time())
+year = time.strftime('%y', now)
+month = time.strftime('%m', now)
+date = time.strftime('%d', now)
+hour = time.strftime('%H', now)
+min = time.strftime('%M', now)
+sec = time.strftime('%S', now)
+
+print('%s년 %s월 %s일' % (year, month, date))
+print('%s시 %s분 %s초' % (hour, min, sec))
+
+# calendar() : 달력 구하는 함수
+c1 = cal.calendar(2020)
+print('c1 : ', c1)
+
 # abs()
+res1 = abs(-5)
+print('res1 : ', res1)
+
 # ceil()
+res2 = math.ceil(1.2)
+res3 = math.ceil(1.8)
+
+print('res2 : ', res2)
+print('res3 : ', res3)
+
+
 # floor()
+res4 = math.floor(1.2)
+res5 = math.floor(1.8)
+
+print('res4 : ', res2)
+print('res5 : ', res3)
+
 # round()
+res6 = round(1.2)
+res7 = round(1.8)
+
+print('res6 : ', res6)
+print('res7 : ', res7)
+
 # random()
+num1 = random.random() # 0~1 사이 실수
+print('num1 : ', num1)
+
+num2 = num1*10
+print('num2 : ', num2) # 1~10 사이 실수
+
+num3 = math.ceil(num2)
+print('num3 : ', num3) # 1~10 사이 정수
+
+num4 = math.ceil(random.random()*10)
+print('num4 : ', num4) # 1~10 사이 실수
